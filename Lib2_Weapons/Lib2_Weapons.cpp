15b8aaae65513e64cdb5c180d3da6fbee7fcf725
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2023-10-24 12:08:20 +0300

    1 Start point.

---
+++ Lib2_Weapons/Lib2_Weapons.cpp
@@ -0,0 +1,10 @@
+// Lib2_Weapons.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnLib2Weapons()
+{
+}
---
+++ Lib3_Defence/Lib3_Defence.cpp
@@ -0,0 +1,10 @@
+// Lib3_Defence.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnLib3Defence()
+{
+}
